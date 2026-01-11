9b4f059d5f258fbc29f4cc6af18b2ef18cd7d57d
Gunjan Jaiswal <gunjan@omsinv.com>
2026-01-11 20:17:32 +0530

    Completed C# Hello World app and refined HTML page

---
+++ program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Welcome " + name);
+    }
+}
+
